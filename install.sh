8f0d93d28d3d0eac2feddb1fedf43ece47b6d248
Jeshua Morrissey <jeshua@clipchamp.com>
2019-10-30 14:48:14 +1000

    add install script

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+PWD="$(pwd)"
+
+# Clean up old things.
+rm -rf "$HOME/.gitconfig" "$HOME/.zsh" "$HOME/.zshrc" "$HOME/bin"
+
+# Make links.
+ln -s "$PWD/.gitconfig" "$HOME/.gitconfig"
+ln -s "$PWD/.zsh" "$HOME/.zsh"
+ln -s "$PWD/.zshrc" "$HOME/.zshrc"
+ln -s "$PWD/bin" "$HOME/bin"
